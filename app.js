a1894539f450df93d39337da00381c14c2d59d53
Glenn <gnel1234@yahoo.com>
2017-10-05 22:34:39 +0000

    Update database

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://Gnel12:Pepper@ds111565.mlab.com:11565/shareyourcamp");
+mongoose.connect("mongodb://<Gnel12>:<Pepper>@ds111565.mlab.com:11565/shareyourcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
