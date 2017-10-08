dcac917cb8a16d1fd0ca6de1d437b08674eab735
Glenn <gnel1234@yahoo.com>
2017-10-08 07:50:35 +0000

    update database

--- app.js
+++ app.js
@@ -15,8 +15,12 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
+
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
     
-mongoose.connect("mongodb://@ds111565.mlab.com:11565/shareyourcamp");
+mongoose.connect(url);
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
