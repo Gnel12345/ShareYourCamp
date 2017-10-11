b64554b44f211a2e91ca576693b6ec86b8f4f42b
Glenn <gnel1234@yahoo.com>
2017-10-11 06:41:48 +0000

    Update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
 
 
 // check the mlab credentials and database hostname/port, probably best to rewatch the lectures
-mongoose.connect("mongodb://G:P@ds115035.mlab.com:15035/shareyourcamp");
+mongoose.connect("https://mlab.com/databases/shareyourcamp#users/glenn");
 //mongoose.connect("mongodb://localhost/yelp_camp");
 
 
