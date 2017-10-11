f22649982f06f29404b90f3784d4a7341d921c97
Glenn <gnel1234@yahoo.com>
2017-10-11 08:20:34 +0000

    update

--- app.js
+++ app.js
@@ -54,5 +54,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+   console.log("The Server Has Started!");
 });
