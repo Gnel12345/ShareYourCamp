0174cd6e12a698d15087c41e213c010a51ca0969
Glenn <gnel1234@yahoo.com>
2018-06-14 09:56:55 +0000

    update

--- public/javascript/dropdown.js
+++ public/javascript/dropdown.js
@@ -1,6 +1,10 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function hoverFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function nothoverFunction(){
+  document.getElementById("myDropdown").classList.toggle("hide");
 }
 
