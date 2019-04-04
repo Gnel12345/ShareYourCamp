be787560c4ab0f19423030589f8b4dc6c6d8bf09
Glenn <gnel1234@yahoo.com>
2019-04-04 08:39:50 +0000

    update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,12 +2,12 @@ var express = require("express");
 var router  = express.Router();
 var Campground = require("../models/campground");
 var middleware = require("../middleware");
-
+require('dotenv').config();
 var NodeGeocoder = require("node-geocoder");
 var options = {
     provider : 'google',
     httpAdapter :'https',
-    apiKey:process.env.GEOCODER_API_KEY,
+    apiKey : process.env.GEOCODER_API_KEY,
     formatter :null
 };
 var geocoder=NodeGeocoder(options);
